b5e770bf6a28b8e9ee6c8babfacb3bebd8caed6d
Milzan Malik <milzan_malik@outlook.com>
2021-10-08 22:25:33 +0700

    build problem7

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static GameManager _instance = null;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: GameManager not Found");
+                }
+            }
+            return _instance;
+        }
+    }
+    #endregion
+    [SerializeField] private TextMeshProUGUI scoreText;
+    public int Score { get; private set; }
+
+
+    void Update()
+    {
+        scoreText.text = Score.ToString();
+    }
+
+    public void setScore()
+    {
+        Score++;
+    }
+}
---
+++ Assets/Scripts/GoldController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldController : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Player")
+        {
+            GameManager.Instance.setScore();
+            Destroy(gameObject);
+        }
+    }
+}
