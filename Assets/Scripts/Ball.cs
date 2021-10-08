2f7e7aaaae256617c7dbca1c6b5e2c10925be0bb
Milzan Malik <milzan_malik@outlook.com>
2021-10-08 21:19:30 +0700

    build problem5

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,11 +7,71 @@ public class Ball : MonoBehaviour
 
     private Rigidbody2D rigidBody2D;
     public float speed;
+    Vector3 movement;
+    int floorMask;
+
+    float camRayLength = 100f;
+
+    [SerializeField]
+    private bool isProblem3;
+    [SerializeField]
+    private bool isProblem4;
+    [SerializeField]
+    private bool isProblem5;
 
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
-        PushBall();
+        if (isProblem3)
+        {
+            PushBall();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Mendapatkan nilai input horizontal (-1,0,1)
+        float h = Input.GetAxisRaw("Horizontal");
+
+        //Mendapatkan nilai input vertical (-1,0,1)
+        float v = Input.GetAxisRaw("Vertical");
+
+        if (isProblem4)
+        {
+            Move(h, v);
+        }
+
+        if (isProblem5)
+        {
+            MouseMove();
+        }
+
+
+    }
+
+    public void Move(float h, float v)
+    {
+        //Set nilai x dan y
+        movement.Set(h, v, 0f);
+
+        //Menormalisasi nilai vector agar total panjang dari vector adalah 1
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        //Move to position
+        rigidBody2D.MovePosition(transform.position + movement);
+    }
+
+    private void MouseMove()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            //ambil posisi mouse
+            movement = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var force = movement - transform.position;
+            force.z = 0;
+            //pindah posisi dengan speed konstan
+            rigidBody2D.velocity = force.normalized * speed;
+        }
     }
 
 
