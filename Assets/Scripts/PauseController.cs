dfce3eb5ecb9712dd97e65f5b5a899bc372d5aa9
Milzan Malik <milzan_malik@outlook.com>
2021-10-10 19:31:24 +0700

    main build

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseController : MonoBehaviour
+{
+    [SerializeField] private Animator Overlay;
+    [SerializeField] private AudioSource escSound;
+    [SerializeField] private AudioSource resumeSound;
+    public static bool gameIsPaused;
+
+    void Update()
+    {
+        if (!gameIsPaused)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+
+                PauseGame();
+                escSound.Play();
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+
+                Resume();
+                resumeSound.Play();
+            }
+        }
+
+    }
+
+
+    public void PauseGame()
+    {
+        Overlay.SetTrigger("Pause");
+        gameIsPaused = true;
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        Overlay.SetTrigger("Unpause");
+        Time.timeScale = 1;
+        gameIsPaused = false;
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void changeScenebyName(string _sceneName)
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+
+    public void changeScenebyID(int _sceneID)
+    {
+        SceneManager.LoadScene(_sceneID);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
